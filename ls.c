15377402b48c2bec4a15d5e67e49d484a5f2766b
Devharsh Trivedi <devharsh@live.in>
2019-09-28 16:34:17 -0400

    modified ls -l command features

--- ls.c
+++ ls.c
@@ -1,4 +1,8 @@
 #include "ls.h"
+#include <dirent.h>
+#include <grp.h>
+#include <pwd.h>
+#include <time.h>
 
 char* permissions(char* file) {
 	struct stat st;
@@ -28,7 +32,12 @@ char* permissions(char* file) {
 int
 main(int argc, char **argv) {
 	DIR *dp;
-	struct dirent *dirp;
+	int aflag = 0;
+	int bflag = 0;
+	char* cvalue = NULL;
+	int index;
+	int c;
+	opterr = 0;
 
 	if (argc != 2) {
 		fprintf(stderr, "usage: %s dir_name\n", argv[0]);
@@ -40,9 +49,82 @@ main(int argc, char **argv) {
 		exit(1);
 	}
 
-	while ((dirp = readdir(dp)) != NULL )
-		printf("%s \t %s\n",permissions(dirp->d_name), dirp->d_name);
+	struct stat st;
+	struct group *grp;
+	struct passwd *pwd;
 
+	int total = 0;
+	
+	struct dirent** dirpa;
+	c = scandir(".", &dirpa, NULL, alphasort);
+	if (c < 0)
+		perror("scandir");
+	else {
+		int k;
+		for(k=0; k<c; k++) {
+			if (stat(dirpa[k]->d_name, &st) != 0)
+    				perror("stat() error");
+			else {
+				grp = getgrgid(st.st_gid);
+				pwd = getpwuid(st.st_uid);
+			
+				total += (int)(st.st_blocks);
+				char buffer[80];
+				struct tm* time_c;
+				time_c = localtime(&st.st_ctime);
+				strftime(buffer, 80, "%b %d %H:%M", time_c);
+			
+				printf("%s\t%d\t%s\t%s\t%ld\t%s\t%s\n",
+				permissions(dirpa[k]->d_name), st.st_nlink,
+				pwd->pw_name, grp->gr_name, 
+				st.st_size, buffer, dirpa[k]->d_name);
+
+				free(dirpa[k]);
+			}
+		}
+		free(dirpa);
+	}
+
+	printf("total = %d\n", total);
+	while ((c = getopt (argc, argv, "abc:")) != -1) {
+		switch (c) {
+			case 'a':
+				aflag = 1;
+				break;
+			case 'b':
+				bflag = 1;
+				break;
+			case 'c':
+				cvalue = optarg;
+				break;
+			case '?':
+				if (optopt == 'c') {
+					fprintf (stderr,
+						 "Option -%c requires an argument.\n",
+						 optopt);
+				}
+				else if (isprint (optopt)) {
+					fprintf (stderr,
+						 "Unknown option '-%c'.\n",
+						 optopt);
+				}
+				else {
+					fprintf (stderr,
+						"Unknown option '-%c' character.\n",
+						optopt);
+				}
+				return 1;
+			default:
+				abort ();
+		}
+	}
+
+	printf ("aflag = %d, bflag = %d, cvalue = %s\n",
+		aflag, bflag, cvalue);
+
+	for (index = optind; index < argc; index++)
+		printf ("Non-option argument %s\n", argv[index]);
+	
 	closedir(dp);
 	return(0);
 }
