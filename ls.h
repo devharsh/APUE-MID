05cffdf68c10428c76aa6db436447cc6dea780af
Devharsh Trivedi <devharsh@live.in>
2019-09-30 17:25:59 -0400

    modified argument for default directory as pwd

--- ls.h
+++ ls.h
@@ -1,10 +1,21 @@
-#include <sys/types.h>
+/*
+ * MID - ls - list the contents of a directory
+ * Author: Devharsh Trivedi
+ * Email: dtrived5@stevens.edu
+*/
+
 #include <sys/stat.h>
+#include <sys/types.h>
 
 #include <ctype.h>
 #include <dirent.h>
 #include <errno.h>
+#include <grp.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
+
+#define PATH_LIM 4096
