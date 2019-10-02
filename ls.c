b1034911e80b769b2e86baf4907c78b391a53a52
Devharsh Trivedi <devharsh@live.in>
2019-10-02 02:15:18 -0400

    made it C90 compatible

--- ls.c
+++ ls.c
@@ -1,5 +1,6 @@
 #include "ls.h"  
 
+
 /*
 * This program simulates the behaviour of a standard ls
 * command on a UNIX like system. It takes the directory
@@ -16,7 +17,8 @@ int main
         	switch(opt) {  
             		case 'i':  
             		case 'l':  
-            		case 'r':  
+				break;
+			case 'r':  
                 		printf("option: %c\n", opt);  
                 		break;  
             		case 'f':  
@@ -38,7 +40,7 @@ int main
       
 	if(!fhit) {
 		char cwd[PATH_LIM];
-                char *c = getcwd(cwd, sizeof(cwd));
+                char* c = getcwd(cwd, sizeof(cwd));
 		if(c == NULL)
 			perror("getcwd");
 		else
@@ -53,15 +55,29 @@ int main
 */
 void
 fts_helper(char *g) {
-        char *pp[] = {g, NULL};
+	int total = 0;
+	int print = 0;
 
-        FTS *ftsp = fts_open(pp, 0, &compare);
+	struct group* grp;
+	struct passwd* pwd;
+	struct tm* time_c;
+
+	char buffer[80];
+	char *modeval = malloc(11);
+	char * pp[2];
+
+	FTS *ftsp;
+
+	pp[0] = g;
+	pp[1] = NULL;
+
+	ftsp = fts_open(pp, 0, &compare);
         if(ftsp == NULL) {
         	perror("fts_open");
         	exit(1);
         }
 
-        while(1) {
+	while(1) {
         	FTSENT *ent = fts_read(ftsp);
         	if(ent == NULL) {
                         if(errno == 0)
@@ -72,7 +88,6 @@ fts_helper(char *g) {
                         }
                 }
 
-		int print = 0; 
 		switch (ent->fts_info) { 
                 	case FTS_D :
 				print = 1;
@@ -85,10 +100,43 @@ fts_helper(char *g) {
                 	default:
                     		break;
             	}
-                
+               
+		/* 
 		if(print && ent->fts_level == 1)
                 	printf("%s\t", ent->fts_name);
-	}       
+		*/
+
+
+
+		grp = getgrgid(ent->fts_statp->st_gid);
+                pwd = getpwuid(ent->fts_statp->st_uid);
+                      
+                total += (int)(ent->fts_statp->st_blocks);
+                time_c = localtime(&ent->fts_statp->st_ctime);
+                strftime(buffer, 80, "%b %d %H:%M", time_c);
+                mode_t perm = ent->fts_statp->st_mode;
+                
+                modeval[0] = (perm & S_IFDIR) ? 'd' : '-';
+                modeval[1] = (perm & S_IRUSR) ? 'r' : '-';
+                modeval[2] = (perm & S_IWUSR) ? 'w' : '-';
+                modeval[3] = (perm & S_IXUSR) ? 'x' : '-';
+                modeval[4] = (perm & S_IRGRP) ? 'r' : '-';
+                modeval[5] = (perm & S_IWGRP) ? 'w' : '-';
+                modeval[6] = (perm & S_IXGRP) ? 'x' : '-';
+                modeval[7] = (perm & S_IROTH) ? 'r' : '-';
+                modeval[8] = (perm & S_IWOTH) ? 'w' : '-';
+                modeval[9] = (perm & S_IXOTH) ? 'x' : '-';
+                modeval[10] = '\0';
+
+		if(print && ent->fts_level == 1)
+                	printf("%s\t%d\t%s\t%s\t%ld\t%s\t%s\n",
+                		modeval, ent->fts_statp->st_nlink,
+                		pwd->pw_name, grp->gr_name, 
+                		ent->fts_statp->st_size, buffer, ent->fts_name);
+                                                        
+	}      
+	
+	printf("total %d\n", total); 
 
         if(fts_close(ftsp) == -1)
         	perror("fts_close");
